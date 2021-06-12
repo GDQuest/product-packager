4011aaa284d2a47e08a6e67ad6fb5621972322ed
Nathan Lovato <nathan@gdquest.com>
2021-06-12 14:41:51 -0600

    Refactor and test table_of_contents.py

--- programs/table_of_contents.py
+++ programs/table_of_contents.py
@@ -9,6 +9,7 @@ import sys
 from dataclasses import dataclass
 import logging
 
+ERROR_COULD_NOT_OPEN_INPUT_FILE: int = 1
 
 LOGGER = logging.getLogger("format_tutorial.py")
 
@@ -23,45 +24,56 @@ class Heading:
 
 
 RE_HEADING: re.Pattern = re.compile(r"^(#+)(.+)$", re.MULTILINE)
-RE_TEMPLATE_CONTENTS: re.Pattern = re.compile(r"^{% contents %}$", re.MULTILINE)
+RE_TEMPLATE_CONTENTS: re.Pattern = re.compile(r"^{% contents %}$", flags=re.MULTILINE)
 
 
-def find_headings(text: str) -> List[Heading]:
-    heading_lines = RE_HEADING.findall(content)
+def find_headings(text: List[str]) -> List[Heading]:
+    heading_lines = [line for line in text if RE_HEADING.search(line) is not None]
     out: List[Heading] = []
     for line in heading_lines:
-        title: str = line.lstrip("# ")
+        # Skip document title
+        if line.startswith("# "):
+            continue
+
+        title: str = line.lstrip("# ").rstrip("\n")
         anchor: str = title.lower().replace(" ", "-")
-        level: int = title.split(" ", 1)[0].count("#")
+        # Subtract 2 so level-2 headings are unindented
+        level: int = line.split(" ", 1)[0].count("#") - 2
         out.append(Heading(title, anchor, level))
     return out
 
 
-def generate_table_of_contents(headings: List[Heading]) -> str:
-    out: List[str] = ["Contents:", ""]
+def generate_table_of_contents(headings: List[Heading], max_level: int = 3) -> List[str]:
+    out: List[str] = ["Contents:\n", "\n"]
     for heading in headings:
-        line = "  " * heading.level + "- [{}]({})".format(heading.title, heading.anchor)
+        if heading.level > max_level:
+            continue
+        line = "  " * heading.level + "- [{}](#{})".format(heading.title, heading.anchor) + "\n"
         out.append(line)
-    return "\n".join(out)
+    return out
 
 
 def main():
-    output: str = ""
-
-    content: str = ""
+    output: List[str] = []
+    content: List[str] = []
     file_path: str = sys.argv[1]
     with open(file_path, "r") as input_file:
-        content = input_file.read()
-    if content == "":
-        LOGGER.error("Could not read file {}. Aborting.".format(file_path))
+        content = input_file.readlines()
+        if not content:
+            LOGGER.error("Could not read file {}. Aborting.".format(file_path))
+            sys.exit(ERROR_COULD_NOT_OPEN_INPUT_FILE)
 
-    match: re.Match = RE_TEMPLATE_CONTENTS.match(content)
-    if match:
-        headings: List[Heading] = find_headings(content)
-        table_of_contents: str = generate_table_of_contents(headings)
-        output = content[:match.start()] + table_of_contents + content[match.end():]
+    index = 0
+    for line in content:
+        match: re.Match = RE_TEMPLATE_CONTENTS.match(line)
+        if match:
+            headings: List[Heading] = find_headings(content)
+            table_of_contents: List[str] = generate_table_of_contents(headings)
+            output = content[:index] + table_of_contents + content[index + 1:]
+            break
+        index += 1
 
-    print(output)
+    print("".join(output))
 
 
 if __name__ == "__main__":
