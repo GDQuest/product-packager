578e37a2a1eeedef1940184447d30575b9c8006d
Nathan Lovato <nathan@gdquest.com>
2021-07-17 10:15:14 -0600

    Update: Refactor scons scripts
    commit dd66b3524e587c0d9c2259e60e3a7f80d2f1c7ac
    Author: Nathan Lovato <nathan@gdquest.com>
    Date:   Sat Jul 17 10:12:13 2021 -0600
    
        Fix: nonexistent function in template Sconstruct file, refactor code
    
    commit 5bfc62822e2d77e09630f52af981f7ed23b56e8e
    Author: Nathan Lovato <nathan@gdquest.com>
    Date:   Thu Jul 15 21:05:40 2021 -0600
    
        Update: inline code from scons_helper, renames
    
    commit e52294c6d6aa650986b0ef6313aa0dac8a2f165d
    Author: Răzvan C. Rădulescu <razcore.rad@gmail.com>
    Date:   Thu Jul 15 22:30:10 2021 +0300
    
        Preliminary clean-up without epub
    
    commit 602a40f6d0eafcdb75b91e38b71ed501d2347843
    Author: Răzvan C. Rădulescu <razcore.rad@gmail.com>
    Date:   Wed Jul 14 21:04:35 2021 +0300
    
        Use `Path` everywhere and remove `as_posix()`

--- programs/scons/scons_helper.py
+++ programs/scons/scons_helper.py
@@ -1,87 +1,50 @@
 import subprocess
+import sys
 from pathlib import Path
 
 import colorama
-
-colorama.init(autoreset=True)
-cwd_base = Path(__file__).parent.absolute().as_posix()
+from SCons.Script import Dir, File
 
 
-def validate_git_version(source_dir: str) -> bool:
+def get_godot_project_files(dir: Dir) -> list[File]:
+    """Return a list of all folders containing a project.godot file"""
+    return [File(p) for p in Path(str(dir)).glob("**/project.godot")]
+
+
+def validate_git_versions(source_dir: Dir) -> bool:
     """Compares git release tags, of root and godot projects to make sure they are identical"""
-    godot_projects = get_godot_folders(source_dir)
-    godot_projects.append(source_dir)
-    result_set = {}
-    for item in godot_projects:
+    godot_project_dirs = [pf.Dir(".") for pf in get_godot_project_files(source_dir)]
+    godot_project_dirs.append(source_dir)
+
+    results = {}
+    for godot_project_dir in godot_project_dirs:
         out = subprocess.run(
-            ["git", "describe", "--tags"], capture_output=True, cwd=item
+            ["git", "describe", "--tags"], capture_output=True, cwd=godot_project_dir
         )
         if out.returncode != 0:
             print_error(out.stderr.decode())
             raise Exception(out.stderr.decode())
-        result_set[Path(item).name] = out.stdout.decode().strip()
-    if len(set(result_set.values())) == 1:
+        results[godot_project_dir.name] = out.stdout.decode().strip()
+
+    ret = False
+    if len(set(results.values())) == 1:
         # All git versions match
-        return True
-    print_error("Multiple git release tags found")
-    for key, value in result_set.items():
+        ret = True
+
+    print_error("WARNING: Multiple git release tags found!")
+    for key, value in results.items():
         print_error(key + " : " + value)
-    return False
+
+    return ret
 
 
-def is_source_directory_valid(source_dir: str) -> bool:
-    """Returns whether the directory contains a content folder"""
-    content_directory = Path(source_dir) / "content"
-    return content_directory.is_dir()
+def print_success(*args, **kwargs):
+    print(colorama.Fore.GREEN, end="", flush=True)
+    print(*args, **kwargs)
+    print(colorama.Fore.RESET, end="", flush=True)
 
 
-def content_introspection(source_dir: str) -> list:
-    """Returns a list of folders within the content folder"""
-    assert (
-        is_source_directory_valid(source_dir),
-        "Source directory lacks a content/ subdirectory.",
-    )
-    content_directory = Path(source_dir) / "content"
-    return [folder for folder in content_directory.iterdir() if folder.is_dir()]
-
-
-def get_godot_folders(root_dir: str) -> list:
-    """Return a list of all folders containing a project.godot file"""
-    projects = [p.parent.as_posix() for p in Path(root_dir).glob("**/project.godot")]
-    return projects
-
-
-def bundle_godot_project(target, source, env):
-    """A SCons Builder script, builds a godot project directory into a zip file"""
-    zipfile_path = Path(target[0].abspath)
-    target_directory = zipfile_path.parent
-    godot_project_directory = Path(source[0].abspath).parent
-    godot_project_name = zipfile_path.stem
-    print_success("Building project %s" % godot_project_name)
-
-    out = subprocess.run(
-        [
-            "./package_godot_project.py",
-            godot_project_directory,
-            "--output",
-            target_directory,
-            "--title",
-            godot_project_name,
-        ],
-        capture_output=True,
-        cwd=cwd_base,
-    )
-    if out.returncode != 0:
-        print_error(out.stderr.decode())
-        raise Exception(out.stderr.decode())
-    if "Done." in out.stdout.decode().split("\n"):
-        print_success("%s built successfully." % godot_project_name)
-    return None
-
-
-def print_success(log_message: str):
-    print(colorama.Fore.GREEN + log_message)
-
-
-def print_error(log_message: str):
-    print(colorama.Fore.RED + log_message)
+def print_error(*args, **kwargs):
+    print(colorama.Fore.RED, end="", flush=True)
+    print(*args, file=sys.stderr, **kwargs)
+    print(colorama.Fore.RESET, end="", flush=True)
