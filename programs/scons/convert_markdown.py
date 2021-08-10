63be2b276027bd182d1988bcaa2ff9972e86da0c
Nathan Lovato <nathan@gdquest.com>
2021-08-10 16:09:53 -0600

    Refactor: run formatter on convert_markdown.py

--- programs/scons/convert_markdown.py
+++ programs/scons/convert_markdown.py
@@ -48,29 +48,46 @@ CONTENT_DIRECTORY: str = "content"
 DEFAULT_CSS_FILE_PATH: Path = Path(THIS_DIRECTORY, "css/pandoc.css")
 DEFAULT_DATA_DIRECTORY: Path = Path(THIS_DIRECTORY, "pandoc")
 
-ERROR_CSS_INVALID: str = "Invalid CSS file. {} is not a valid file. Using default path {}."
+ERROR_CSS_INVALID: str = (
+    "Invalid CSS file. {} is not a valid file. Using default path {}."
+)
 PDF_ENGINE_MEMBERS = [member.value for member in PdfEngines]
-HELP_PDF_ENGINE = ["PDF rendering engine to use if --type is pdf.",
-                   "Supported PDF rendering engines: {}".format(", ".join(PDF_ENGINE_MEMBERS))]
+HELP_PDF_ENGINE = [
+    "PDF rendering engine to use if --type is pdf.",
+    "Supported PDF rendering engines: {}".format(", ".join(PDF_ENGINE_MEMBERS)),
+]
 HELP_CSS_FILE = "Path to the css file to use for rendering. Default: {}".format(
-    DEFAULT_CSS_FILE_PATH)
+    DEFAULT_CSS_FILE_PATH
+)
 
 
 @dataclass
 class Args:
-    files: Sequence[Path] = arg(positional=True,
-                                help="A list of paths to markdown files.")
+    files: Sequence[Path] = arg(
+        positional=True, help="A list of paths to markdown files."
+    )
     output_directory: Path = arg(
-        default=Path(), help="Path to the output directory.", aliases=["-o"])
+        default=Path(), help="Path to the output directory.", aliases=["-o"]
+    )
     pdf_engine: PdfEngines = arg(
-        default=PdfEngines.weasyprint, help="\n".join(HELP_PDF_ENGINE), aliases=["-p"])
-    output_type: OutputTypes = arg(default=OutputTypes.html,
-                                   help="Type of file to output, either html or pdf.", aliases=["-t"])
+        default=PdfEngines.weasyprint, help="\n".join(HELP_PDF_ENGINE), aliases=["-p"]
+    )
+    output_type: OutputTypes = arg(
+        default=OutputTypes.html,
+        help="Type of file to output, either html or pdf.",
+        aliases=["-t"],
+    )
     css: Path = arg(default=DEFAULT_CSS_FILE_PATH, help=HELP_CSS_FILE, aliases=["-c"])
     pandoc_data_directory: Path = arg(
-        default=DEFAULT_DATA_DIRECTORY, help="Path to a data directory to use for pandoc.", aliases=["-d"])
-    filters: Sequence[str] = arg(default=(), aliases=['-f'],
-                                 help="List of pandoc filters to run on each content file.")
+        default=DEFAULT_DATA_DIRECTORY,
+        help="Path to a data directory to use for pandoc.",
+        aliases=["-d"],
+    )
+    filters: Sequence[str] = arg(
+        default=(),
+        aliases=["-f"],
+        help="List of pandoc filters to run on each content file.",
+    )
 
 
 def path_to_title(filepath: str) -> str:
@@ -92,9 +109,17 @@ def convert_markdown(args: Args, path: str) -> None:
     """Builds and runs a pandoc command to convert the input markdown document
     `path` to the desired output format."""
     title: str = path_to_title(path)
-    pandoc_command = ["pandoc", path.absolute().as_posix(), "--self-contained", "--css", args.css.absolute().as_posix(),
-                      "--metadata", "pagetitle='{}'".format(title), "--data-dir",
-                      args.pandoc_data_directory.absolute().as_posix()]
+    pandoc_command = [
+        "pandoc",
+        path.absolute().as_posix(),
+        "--self-contained",
+        "--css",
+        args.css.absolute().as_posix(),
+        "--metadata",
+        "pagetitle='{}'".format(title),
+        "--data-dir",
+        args.pandoc_data_directory.absolute().as_posix(),
+    ]
     if args.output_type == OutputTypes.pdf:
         pandoc_command += ["--pdf-engine", args.pdf_engine]
     if args.filters:
@@ -104,10 +129,10 @@ def convert_markdown(args: Args, path: str) -> None:
     # To use pandoc's built-in syntax highlighter. The theme still needs some work.
     # PANDOC_DIRECTORY: Path = Path(THIS_DIRECTORY, "pandoc")
     # pandoc_command += [
-        # "--syntax-definition",
-        # Path(PANDOC_DIRECTORY, "gd-script.xml").absolute().as_posix(),
-        # "--highlight-style",
-        # Path(PANDOC_DIRECTORY, "gdscript.theme").absolute().as_posix()
+    # "--syntax-definition",
+    # Path(PANDOC_DIRECTORY, "gd-script.xml").absolute().as_posix(),
+    # "--highlight-style",
+    # Path(PANDOC_DIRECTORY, "gdscript.theme").absolute().as_posix()
     # ]
 
     if not output_path.parent.exists():
