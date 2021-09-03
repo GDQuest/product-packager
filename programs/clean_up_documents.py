57c52b0a789c4bc6516001d7ef891f2b41f2b5d6
Nathan Lovato <nathan@gdquest.com>
2021-09-03 15:33:13 -0600

    improvement: remove more unwanted words in clean_up_documents

--- programs/clean_up_documents.py
+++ programs/clean_up_documents.py
@@ -9,25 +9,61 @@ translated words.
 import sys
 import re
 
-def filter_out_code(file_path: str) -> str:
+
+def remove_front_matter(content: str) -> str:
+    """Removes front matter from markdown content"""
+    return re.sub(r"^---\n(.*?)\n---", "", content, flags=re.S)
+
+
+def remove_markdown_images(content: str) -> str:
+    """Removes lines with markdown image tags from the content"""
+    return re.sub(r"\!\[.*?\]\(.*?\)\n?", "", content)
+
+
+def remove_hash_marks(content: str) -> str:
+    """Removes hash marks from the content"""
+    return re.sub("^#+ ", "", content, flags=re.M)
+
+
+def remove_templates(content: str) -> str:
+    """Removes markdown templates from the content"""
+    return re.sub("\{\%.*\%\}", "", content)
+
+
+def remove_list_items(content: str) -> str:
+    """Removes list items from the content"""
+    return re.sub("^(\d+\.)|(-) ", "", content, flags=re.M)
+
+
+def merge_line_returns(content: str) -> str:
+    """Merges line returns into a single line"""
+    return re.sub("\n\n+", "\n\n", content)
+
+
+def filter_out_code(content: str) -> str:
     """Finds code blocks in the markdown document"""
 
     def filter_code_comments(match):
         lines = match.group(2).split("\n")
         return "\n".join([line for line in lines if "# " in line])
 
-    with open(file_path, "r") as md_file:
-        return re.sub(
-            "```([a-z]*)\n(.*?)```", filter_code_comments, md_file.read(), flags=re.S
-        )
+    return re.sub("```([a-z]*)\n(.*?)```", filter_code_comments, content, flags=re.S)
 
 
 def main():
     filepaths = [arg for arg in sys.argv if arg.endswith(".md")]
     for filepath in filepaths:
-        with open(filepath.replace('.md', '') + "_out.md", 'w') as output_file:
-            output_file.write(filter_out_code(filepath))
+        with open(filepath, "r") as md_file:
+            content = md_file.read()
+            content = remove_front_matter(content)
+            content = filter_out_code(content)
+            content = remove_markdown_images(content)
+            content = remove_hash_marks(content)
+            content = remove_list_items(content)
+            content = remove_templates(content)
+            content = merge_line_returns(content)
+            print(content.strip())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
