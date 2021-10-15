89811fa7f62e400bc158012a526d5150f52c1e46
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2021-10-15 16:53:04 +0300

    feat(i18n): po4a test for gdscript files

---
+++ programs/i18n/test/README.md
@@ -0,0 +1,4 @@
+# Locale::Po4a::Gdscriptcomments implementation
+
+From this folder, run with `PERL5LIB=$PWD/../perl5:$PERL5LIB po4a po4a.conf`.
+
---
+++ programs/i18n/test/test.gd
@@ -0,0 +1,14 @@
+## Testing comments
+##
+## More comments
+## On multiple lines.
+func _ready() -> void:
+  # Inner comments
+  pass
+  # Inner comments
+
+# Final comment
+
+# More comments.
+# Inner comments.
+# Long line with a lot of comments that goes beyond the number to be wrapped.
