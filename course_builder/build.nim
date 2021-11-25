18ed25f2b0c9313517a66f68257005d5aed07320
Nathan Lovato <nathan@gdquest.com>
2021-11-24 20:09:58 -0600

    refactor: start rewrite course builder in nim

---
+++ course_builder/build.nim
@@ -0,0 +1,27 @@
+import os
+import strutils
+import markdown_preprocessors
+
+const
+    ContentDirectory = "content"
+    OutputDirectory = "dist"
+
+proc findAllMarkdownFiles(): seq[string] =
+    for entry in walkDirRec(ContentDirectory):
+        if entry.endsWith(".md"):
+            result.add(entry)
+
+proc convertMarkdownToHtml(filePath: string, outputDir: string): void =
+    let fileName = filePath.extractFilename()
+    let outputFilePath = outputDir / fileName.changeFileExt(".html")
+    let document = readFile(filePath).runMarkdownPreprocessors()
+    # TODO: replace icons
+    # TODO: Run pandoc command to output the file
+
+let markdownFiles = findAllMarkdownFiles()
+for file in markdownFiles:
+    convertMarkdownToHtml(file, OutputDirectory)
+
+when isMainModule:
+    if not dirExists(OutputDirectory):
+        createDir(OutputDirectory)
