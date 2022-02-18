GDQuest/product-packager

commit 03992c4f4e78cca38e17fff37d3caf3c1279aabe

feat(preprocess): add logic for shortcode in paragraph lines

src/format.nim
@@ -15,54 +15,54 @@ import std/strutils
 import std/sugar
 import std/wordwrap
 import md/godotbuiltins
-import md/parser as md
+import md/parser as mdp
 
 
 const
-    SupportedExtensions = ["png", "jpe?g", "mp4", "mkv", "t?res", "t?scn", "gd", "py", "shader", ]
-    PatternFilenameOnly = r"(\w+\.(" & SupportedExtensions.join("|") & "))"
-    PatternDirPath = r"((res|user)://)?/?([\w]+/)+(\w*\.\w+)?"
-    PatternFileAtRoot = r"(res|user)://\w+\.\w+"
-    PatternModifierKeys = r"Ctrl|Alt|Shift|CTRL|ALT|SHIFT|Super|SUPER"
-    PatternKeyboardSingleCharacterKey = r"[A-Z0-9!@#$%^&*()_\-{}|\[\]\\;':,\./<>?]"
-    PatternOtherKeys = r"F\d{1,2}|Tab|Up|Down|Left|Right|LMB|MMB|RMB|Backspace|Delete"
-    PatternFunctionOrConstructorCall = r"\w+(\.\w+)*\(.*?\)"
-    PatternVariablesAndProperties = r"_\w+|[a-zA-Z0-9]+([\._]\w+)+"
-    PatternGodotBuiltIns = GodotBuiltInClassesByLength.join("|")
+  SupportedExtensions = ["png", "jpe?g", "mp4", "mkv", "t?res", "t?scn", "gd", "py", "shader", ]
+  PatternFilenameOnly = r"(\w+\.(" & SupportedExtensions.join("|") & "))"
+  PatternDirPath = r"((res|user)://)?/?([\w]+/)+(\w*\.\w+)?"
+  PatternFileAtRoot = r"(res|user)://\w+\.\w+"
+  PatternModifierKeys = r"Ctrl|Alt|Shift|CTRL|ALT|SHIFT|Super|SUPER"
+  PatternKeyboardSingleCharacterKey = r"[A-Z0-9!@#$%^&*()_\-{}|\[\]\\;':,\./<>?]"
+  PatternOtherKeys = r"F\d{1,2}|Tab|Up|Down|Left|Right|LMB|MMB|RMB|Backspace|Delete"
+  PatternFunctionOrConstructorCall = r"\w+(\.\w+)*\(.*?\)"
+  PatternVariablesAndProperties = r"_\w+|[a-zA-Z0-9]+([\._]\w+)+"
+  PatternGodotBuiltIns = GodotBuiltInClassesByLength.join("|")
 
 let
-    RegexFilePath = re([PatternDirPath, PatternFileAtRoot, PatternFilenameOnly].join("|"))
-    RegexStartOfList = re"\s*(- |\d+\. )"
-    RegexMaybeCodeCommentLine = re"\s*#*"
-    RegexOnePascalCaseWord = re"[A-Z0-9]\w+[A-Z]\w+|[A-Z][a-zA-Z0-9]+(\.\.\.)?"
-    RegexOnePascalCaseWordStrict = re"[A-Z0-9]\w+[A-Z]\w+"
-    RegexMenuOrPropertyEntry = re"[A-Z0-9]+[a-zA-Z0-9]*( (-> )+[A-Z][a-zA-Z0-9]+( [A-Z][a-zA-Z0-9]*)*(\.\.\.)?)+"
-    RegexCapitalWordSequence = re"[A-Z0-9]+[a-zA-Z0-9]*( (-> )?[A-Z][a-zA-Z0-9]+(\.\.\.)?)+"
-    RegexKeyboardShortcut = re("((" & PatternModifierKeys & r") ?\+ ?)+(" & PatternOtherKeys & "|" & PatternKeyboardSingleCharacterKey & ")")
-    RegexOneKeyboardKey = re("(" & [PatternModifierKeys, PatternOtherKeys, PatternKeyboardSingleCharacterKey].join("|") & ")")
-    RegexNumber = re"\d+(D|px)|\d+(x\d+)*"
-    RegexHexValue = re"(0x|#)[0-9a-fA-F]+"
-    RegexCodeIdentifier = re([PatternFunctionOrConstructorCall, PatternVariablesAndProperties].join("|"))
-    RegexGodotBuiltIns = re(PatternGodotBuiltIns)
-    RegexSkip = re"""({%.*?%}|_.+?_|\*\*[^*]+?\*\*|\*[^*]+?\*|`.+?`|".+?"|'.+?'|\!?\[.+?\)|\[.+?\])\s*|\s+|$"""
-    RegexStartOfSentence = re"\s*\p{Lu}"
-    RegexEndOfSentence = re"[.!?:]\s+"
-    RegexFourSpaces = re" {4}"
-    RegexAtLeastThreeHashes = re"#{3,}"
+  RegexFilePath = re([PatternDirPath, PatternFileAtRoot, PatternFilenameOnly].join("|"))
+  RegexStartOfList = re"\s*(- |\d+\. )"
+  RegexMaybeCodeCommentLine = re"\s*#*"
+  RegexOnePascalCaseWord = re"[A-Z0-9]\w+[A-Z]\w+|[A-Z][a-zA-Z0-9]+(\.\.\.)?"
+  RegexOnePascalCaseWordStrict = re"[A-Z0-9]\w+[A-Z]\w+"
+  RegexMenuOrPropertyEntry = re"[A-Z0-9]+[a-zA-Z0-9]*( (-> )+[A-Z][a-zA-Z0-9]+( [A-Z][a-zA-Z0-9]*)*(\.\.\.)?)+"
+  RegexCapitalWordSequence = re"[A-Z0-9]+[a-zA-Z0-9]*( (-> )?[A-Z][a-zA-Z0-9]+(\.\.\.)?)+"
+  RegexKeyboardShortcut = re("((" & PatternModifierKeys & r") ?\+ ?)+(" & PatternOtherKeys & "|" & PatternKeyboardSingleCharacterKey & ")")
+  RegexOneKeyboardKey = re("(" & [PatternModifierKeys, PatternOtherKeys, PatternKeyboardSingleCharacterKey].join("|") & ")")
+  RegexNumber = re"\d+(D|px)|\d+(x\d+)*"
+  RegexHexValue = re"(0x|#)[0-9a-fA-F]+"
+  RegexCodeIdentifier = re([PatternFunctionOrConstructorCall, PatternVariablesAndProperties].join("|"))
+  RegexGodotBuiltIns = re(PatternGodotBuiltIns)
+  RegexSkip = re"""({%.*?%}|_.+?_|\*\*[^*]+?\*\*|\*[^*]+?\*|`.+?`|".+?"|'.+?'|\!?\[.+?\)|\[.+?\])\s*|\s+|$"""
+  RegexStartOfSentence = re"\s*\p{Lu}"
+  RegexEndOfSentence = re"[.!?:]\s+"
+  RegexFourSpaces = re" {4}"
+  RegexAtLeastThreeHashes = re"#{3,}"
 
 
 func regexWrap(regexes: seq[Regex], pair: (string, string)): string -> (string, string)
 func regexWrapEach(regexAll, regexOne: Regex; pair: (string, string)): string -> (string, string)
 
 let formatters =
-    { "any": regexWrapEach(RegexKeyboardShortcut, RegexOneKeyboardKey, ("<kbd>", "</kbd>"))
-    , "any": regexWrap(@[RegexFilePath], ("`", "`"))
-    , "any": regexWrap(@[RegexMenuOrPropertyEntry], ("*", "*"))
-    , "any": regexWrap(@[RegexCodeIdentifier, RegexGodotBuiltIns], ("`", "`"))
-    , "any": regexWrap(@[RegexNumber, RegexHexValue], ("`", "`"))
-    , "any": regexWrap(@[RegexOnePascalCaseWordStrict], ("*", "*"))
-    , "skipStartOfSentence": regexWrap(@[RegexCapitalWordSequence, RegexOnePascalCaseWord], ("*", "*"))
-    }
+  { "any": regexWrapEach(RegexKeyboardShortcut, RegexOneKeyboardKey, ("<kbd>", "</kbd>"))
+  , "any": regexWrap(@[RegexFilePath], ("`", "`"))
+  , "any": regexWrap(@[RegexMenuOrPropertyEntry], ("*", "*"))
+  , "any": regexWrap(@[RegexCodeIdentifier, RegexGodotBuiltIns], ("`", "`"))
+  , "any": regexWrap(@[RegexNumber, RegexHexValue], ("`", "`"))
+  , "any": regexWrap(@[RegexOnePascalCaseWordStrict], ("*", "*"))
+  , "skipStartOfSentence": regexWrap(@[RegexCapitalWordSequence, RegexOnePascalCaseWord], ("*", "*"))
+  }
 
 
 const HelpMessage = """Auto-formats markdown documents, saving manual formatting work:
@@ -88,187 +88,184 @@ Options:
 
 
 type
-    CommandLineArgs = object
-        inputFiles: seq[string]
-        inPlace: bool
-        outputDirectory: string
+  CommandLineArgs = object
+    inputFiles: seq[string]
+    inPlace: bool
+    outputDirectory: string
 
 
 func regexWrap(regexes: seq[Regex], pair: (string, string)): string -> (string, string) =
-    ## Retruns a function that takes the string `text` and finds the first
-    ## regex match from `regexes` only at the beginning of `text`.
-    ##
-    ## The function returns a tuple:
-    ## - `(text wrapped by pair, rest of text)` if there's a regex match.
-    ## - `("", text)` if there's no regex match.
-    return func(text: string): (string, string) =
-        for regex in regexes:
-            let bound = text.matchLen(regex)
-            if bound == -1: continue
-            return (pair[0] & text[0 ..< bound] & pair[1], text[bound .. ^1])
-        return ("", text)
+  ## Retruns a function that takes the string `text` and finds the first
+  ## regex match from `regexes` only at the beginning of `text`.
+  ##
+  ## The function returns a tuple:
+  ## - `(text wrapped by pair, rest of text)` if there's a regex match.
+  ## - `("", text)` if there's no regex match.
+  return func(text: string): (string, string) =
+    for regex in regexes:
+      let bound = text.matchLen(regex)
+      if bound == -1: continue
+      return (pair[0] & text[0 ..< bound] & pair[1], text[bound .. ^1])
+    return ("", text)
 
 
 func regexWrapEach(regexAll, regexOne: Regex; pair: (string, string)): string -> (string, string) =
-    ## Returns a function that takes the string `text` and tries to match
-    ## `regexAll` only at the beginning of `text`.
-    ##
-    ## The function regurns a tuple:
-    ## - `(text with all occurances of RegexOne wrapped by pair, rest of text)`
-    ##   if there's a regex match.
-    ## - `("", text)` if there's no regex match.
-    return func(text: string): (string, string) =
-        let bound = text.matchLen(regexAll)
-        if bound != -1:
-            let replaced = replacef(text[0 ..< bound], regexOne, pair[0] & "$1" & pair[1])
-            return (replaced, text[bound .. ^1])
-        return ("", text)
+  ## Returns a function that takes the string `text` and tries to match
+  ## `regexAll` only at the beginning of `text`.
+  ##
+  ## The function regurns a tuple:
+  ## - `(text with all occurances of RegexOne wrapped by pair, rest of text)`
+  ##   if there's a regex match.
+  ## - `("", text)` if there's no regex match.
+  return func(text: string): (string, string) =
+    let bound = text.matchLen(regexAll)
+    if bound != -1:
+      let replaced = replacef(text[0 ..< bound], regexOne, pair[0] & "$1" & pair[1])
+      return (replaced, text[bound .. ^1])
+    return ("", text)
 
 
 proc formatLine(line: string): string =
-    ## Returns the formatted `line` using the GDQuest standard.
+  ## Returns the formatted `line` using the GDQuest standard.
 
-    proc advance(line: string): (string, string) =
-        ## Find the first `RegexSkip` and split `line` into the tuple
-        ## `(line up to RegexSkip, rest of line)`.
-        let (_, last) = line.findBounds(RegexSkip)
-        (line[0 .. last], line[last + 1 .. ^1])
+  proc advance(line: string): (string, string) =
+    ## Find the first `RegexSkip` and split `line` into the tuple
+    ## `(line up to RegexSkip, rest of line)`.
+    let (_, last) = line.findBounds(RegexSkip)
+    (line[0 .. last], line[last + 1 .. ^1])
 
-    block outer:
-        var
-            line = line
-            isStartOfSentence = line.startsWith(RegexStartOfSentence)
+  block outer:
+    var
+      line = line
+      isStartOfSentence = line.startsWith(RegexStartOfSentence)
 
-        while true:
-            for (applyAt, formatter) in formatters:
-                if line.len <= 0: break outer
-                if (applyAt, isStartOfSentence) == ("skipStartOfSentence", true): continue
-                let (formatted, rest) = formatter(line)
-                result &= formatted
-                line = rest
+    while true:
+      for (applyAt, formatter) in formatters:
+        if line.len <= 0: break outer
+        if (applyAt, isStartOfSentence) == ("skipStartOfSentence", true): continue
+        let (formatted, rest) = formatter(line)
+        result &= formatted
+        line = rest
 
-            let (advanced, rest) = advance(line)
-            isStartOfSentence = advanced.endsWith(RegexEndOfSentence)
-            result &= advanced
-            line = rest
+      let (advanced, rest) = advance(line)
+      isStartOfSentence = advanced.endsWith(RegexEndOfSentence)
+      result &= advanced
+      line = rest
 
 
 proc formatList(lines: seq[string]): seq[string] =
-    ## Returns the formatted sequence of `lines` using the GDQuest standard.
-    ##
-    ## `lines` is a sequence representing a markdown list. One item can span
-    ## multiple lines, in which case they get concatenated before formatting.
-    var
-        lines = lines
-        linesStart: seq[string]
-        i = 0
+  ## Returns the formatted sequence of `lines` using the GDQuest standard.
+  ##
+  ## `lines` is a sequence representing a markdown list. One item can span
+  ## multiple lines, in which case they get concatenated before formatting.
+  var
+    lines = lines
+    linesStart: seq[string]
+    i = 0
 
-    while i < lines.len:
-        let bound = lines[i].matchLen(RegexStartOfList)
-        if bound != -1:
-            linesStart.add(lines[i][0 ..< bound])
-            lines[i] = lines[i][bound .. ^1]
-            i.inc
-        else:
-            lines[i - 1] &= " " & lines[i].strip()
-            lines.delete(i)
-
-    for (lineStart, line) in zip(linesStart, lines):
-        result.add(lineStart & line.formatLine)
-
-
-
-proc formatCodeLine(codeLine: md.CodeLine): string =
-    ## Returns the formatted `codeLine` block using the GDQuest standard:
-    ##
-    ## - Converts space-based indentations to tabs.
-    ## - Fills GDScript code comments as paragraphs with a max line length of 80.
-    case codeLine.kind
-    of md.clkGDQuestShortcode:
-        codeLine.gdquestShortcode.render
-
-    of md.clkRegular:
-        const (TAB, HASH, MAX_LINE_LEN) = ("\t", '#', 80)
-        let
-            bound = max(0, codeLine.line.matchLen(RegexMaybeCodeCommentLine))
-            indent = codeLine.line[0 ..< bound].multiReplace(
-                [ (RegexFourSpaces, TAB)
-                , (RegexAtLeastThreeHashes, HASH.repeat(2))
-                ])
-            sep = if indent.endsWith(HASH): " " else: ""
-            margin = indent.count(TAB) + indent.strip.len + sep.len
-            wrapLen = if sep == "": codeLine.line.len else: MAX_LINE_LEN - margin
-
-        codeLine.line[bound .. ^1]
-            .strip.wrapWords(wrapLen, splitLongWords=false)
-            .splitLines.map(x => [indent, x].join(sep))
-            .join(md.NL)
-
-
-proc formatBlock(mdBlock: md.Block): string =
-    ## Takes an `mdBlock` from a parsed markdown file
-    ## and returns a formatted string.
-    var partialResult: seq[string]
-
-    case mdBlock.kind
-    of md.bkCode:
-        const OPEN_CLOSE = "```"
-        partialResult.add(OPEN_CLOSE & mdBlock.language)
-        partialResult &= mdBlock.code.map(formatCodeLine)
-        partialResult.add(OPEN_CLOSE)
-
-    of md.bkList:
-        partialResult &= mdBlock.body.formatList
-
-    of md.bkParagraph:
-        partialResult &= mdBlock.body.map(formatLine)
-
+  while i < lines.len:
+    let bound = lines[i].matchLen(RegexStartOfList)
+    if bound != -1:
+      linesStart.add(lines[i][0 ..< bound])
+      lines[i] = lines[i][bound .. ^1]
+      i.inc
     else:
-        partialResult.add(mdBlock.render)
+      lines[i - 1] &= mdp.SPACE & lines[i].strip()
+      lines.delete(i)
 
-    partialResult.join(md.NL)
+  for (lineStart, line) in zip(linesStart, lines):
+    result.add(lineStart & line.formatLine)
+
+
+
+proc formatCodeLine(codeLine: mdp.CodeLine): string =
+  ## Returns the formatted `codeLine` block using the GDQuest standard:
+  ##
+  ## - Converts space-based indentations to tabs.
+  ## - Fills GDScript code comments as paragraphs with a max line length of 80.
+  case codeLine.kind
+  of mdp.clkShortcode:
+    codeLine.shortcode.render
+
+  of mdp.clkRegular:
+    const (TAB, HASH, MAX_LINE_LEN) = ("\t", '#', 80)
+    let
+      bound = max(0, codeLine.line.matchLen(RegexMaybeCodeCommentLine))
+      indent = codeLine.line[0 ..< bound].multiReplace(
+        [ (RegexFourSpaces, TAB)
+        , (RegexAtLeastThreeHashes, HASH.repeat(2))
+        ])
+      sep = if indent.endsWith(HASH): mdp.SPACE else: ""
+      margin = indent.count(TAB) + indent.strip.len + sep.len
+      wrapLen = if sep == "": codeLine.line.len else: MAX_LINE_LEN - margin
+
+    codeLine.line[bound .. ^1]
+      .strip.wrapWords(wrapLen, splitLongWords=false)
+      .splitLines.map(x => [indent, x].join(sep))
+      .join(mdp.NL)
+
+
+proc formatBlock(mdBlock: mdp.Block): string =
+  ## Takes an `mdBlock` from a parsed markdown file
+  ## and returns a formatted string.
+  var partialResult: seq[string]
+
+  case mdBlock.kind
+  of mdp.bkCode:
+    const OPEN_CLOSE = "```"
+    partialResult.add(OPEN_CLOSE & mdBlock.language)
+    partialResult &= mdBlock.code.map(formatCodeLine)
+    partialResult.add(OPEN_CLOSE)
+
+  of mdp.bkList:
+    partialResult &= mdBlock.body.formatList
+
+  else:
+    partialResult.add(mdBlock.render)
+
+  partialResult.join(mdp.NL)
 
 
 
 proc formatContent*(content: string): string =
-    ## Takes the markdown `content` and returns a formatted document using
-    ## the GDQuest standard.
-    md.parse(content).map(formatBlock).join(md.NL).strip & md.NL
+  ## Takes the markdown `content` and returns a formatted document using
+  ## the GDQuest standard.
+  mdp.parse(content).map(formatBlock).join(mdp.NL).strip & mdp.NL
 
 
 proc parseCommandLineArguments(): CommandLineArgs =
-    for kind, key, value in getopt():
-        case kind
-        of cmdEnd: break
-        of cmdArgument:
-            if fileExists(key):
-                result.inputFiles.add(key)
-            else:
-                echo "Invalid filename: ", key
-                quit(1)
-        of cmdLongOption, cmdShortOption:
-            case key
-            of "help", "h": echo HelpMessage
-            of "in-place", "i": result.inPlace = true
-            of "output-directory", "o":
-                if isValidFilename(value):
-                    result.outputDirectory = value
-                else:
-                    echo ["Invalid output directory: ", value, "", HelpMessage].join(md.NL)
-                    quit(1)
-            else:
-                echo ["Invalid option: ", key, "", HelpMessage].join(md.NL)
-                quit(1)
-    if result.inputFiles.len() == 0:
-        echo ["No input files specified.", "", HelpMessage].join(md.NL)
+  for kind, key, value in getopt():
+    case kind
+    of cmdEnd: break
+    of cmdArgument:
+      if fileExists(key):
+        result.inputFiles.add(key)
+      else:
+        echo "Invalid filename: ", key
         quit(1)
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "help", "h": echo HelpMessage
+      of "in-place", "i": result.inPlace = true
+      of "output-directory", "o":
+        if isValidFilename(value):
+          result.outputDirectory = value
+        else:
+          echo ["Invalid output directory: ", value, "", HelpMessage].join(mdp.NL)
+          quit(1)
+      else:
+        echo ["Invalid option: ", key, "", HelpMessage].join(mdp.NL)
+        quit(1)
+  if result.inputFiles.len() == 0:
+    echo ["No input files specified.", "", HelpMessage].join(mdp.NL)
+    quit(1)
 
 
 when isMainModule:
-    var args = parseCommandLineArguments()
-    for file in args.inputFiles:
-        let formattedContent = readFile(file).formatContent
-        if args.inPlace:
-            writeFile(file, formattedContent)
-        else:
-            echo formattedContent
+  var args = parseCommandLineArguments()
+  for file in args.inputFiles:
+    let formattedContent = readFile(file).formatContent
+    if args.inPlace:
+      writeFile(file, formattedContent)
+    else:
+      echo formattedContent

src/build.nim
@@ -3,25 +3,25 @@ import strutils
 import md/preprocess
 
 const
-    ContentDirectory = "content"
-    OutputDirectory = "dist"
+  ContentDirectory = "content"
+  OutputDirectory = "dist"
 
 proc findAllMarkdownFiles(): seq[string] =
-    for entry in walkDirRec(ContentDirectory):
-        if entry.endsWith(".md"):
-            result.add(entry)
+  for entry in walkDirRec(ContentDirectory):
+    if entry.endsWith(".md"):
+      result.add(entry)
 
 proc convertMarkdownToHtml(filePath: string, outputDir: string): void =
-    let fileName = filePath.extractFilename()
-    let outputFilePath = outputDir / fileName.changeFileExt(".html")
-    let document = readFile(filePath).runMarkdownPreprocessors()
-    # TODO: replace icons
-    # TODO: Run pandoc command to output the file
+  let fileName = filePath.extractFilename()
+  let outputFilePath = outputDir / fileName.changeFileExt(".html")
+  let document = readFile(filePath).runMarkdownPreprocessors()
+  # TODO: replace icons
+  # TODO: Run pandoc command to output the file
 
 let markdownFiles = findAllMarkdownFiles()
 for file in markdownFiles:
-    convertMarkdownToHtml(file, OutputDirectory)
+  convertMarkdownToHtml(file, OutputDirectory)
 
 when isMainModule:
-    if not dirExists(OutputDirectory):
-        createDir(OutputDirectory)
+  if not dirExists(OutputDirectory):
+    createDir(OutputDirectory)

src/md/parser.nim
@@ -1,10 +1,10 @@
 import std/sequtils
-import std/strformat
 import std/strutils
 import std/sugar
 import honeycomb
 
 
+const SPACE* = " "
 const NL* = "\n"
 
 type
@@ -12,7 +12,7 @@ type
     bkBlank = "Blank",
     bkHeading = "Heading",
     bkCode = "Code",
-    bkGDQuestShortcode = "GDQuestShortcode",
+    bkShortcode = "Shortcode",
     bkImage = "Image",
     bkParagraph = "Paragraph",
     bkList = "List",
@@ -32,7 +32,7 @@ type
     of bkImage:
       alt*: string
       path*: string
-    of bkGDQuestShortcode:
+    of bkShortcode:
       name*: string
       args*: seq[string]
     of bkParagraph .. bkHTML:
@@ -41,47 +41,45 @@ type
 
   CodeLineKind* = enum
     clkRegular = "Regular",
-    clkGDQuestShortcode = "GDQuestShortcode"
+    clkShortcode = "Shortcode"
 
   CodeLine* = object
     case kind*: CodeLineKind
-    of clkGDQuestShortcode: gdquestShortcode*: Block
+    of clkShortcode: shortcode*: Block
     of clkRegular: line*: string
-
-
-func CodeRegular(line: string): CodeLine = CodeLine(kind: clkRegular, line: line)
-func CodeGDQuestShortcode(gdquestShortcode: Block): CodeLine =
-  if gdquestShortcode.kind != bkGDQuestShortcode:
-    raise newException(ValueError, "Only bkGDQuestShortcode is allowed, but got {gdquestShortcode.kind}".fmt)
-  CodeLine(kind: clkGDQuestShortcode, gdquestShortcode: gdquestShortcode)
 
 func render*(b: Block): string
 
+func CodeRegular*(line: string): CodeLine = CodeLine(kind: clkRegular, line: line)
+func CodeShortcode*(shortcode: Block): CodeLine = CodeLine(kind: clkShortcode, shortcode: shortcode)
+
 func render*(cl: CodeLine): string =
   case cl.kind
-    of clkGDQuestShortcode: cl.gdquestShortcode.render
+    of clkShortcode: cl.shortcode.render
     of clkRegular: cl.line
 
 
-func Blank(): Block = Block(kind: bkBlank)
-func Heading(level: int, heading: string): Block = Block(kind: bkHeading, level: level, heading: heading)
-func Code(language: string, code: seq[CodeLine]): Block = Block(kind: bkCode, language: if language.strip == "": "gdscript" else: language, code: code)
-func Image(alt: string, path: string): Block = Block(kind: bkImage, alt: alt, path: path)
-func GDQuestShortcode(name: string, args: seq[string]): Block = Block(kind: bkGDQuestShortcode, name: name, args: args)
-func Paragraph(body: seq[string]): Block = Block(kind: bkParagraph, body: body)
-func List(body: seq[string]): Block = Block(kind: bkList, body: body)
-func BlockQuote(body: seq[string]): Block = Block(kind: bkBlockQuote, body: body)
-func YAMLFrontMatter(body: seq[string]): Block = Block(kind: bkYAMLFrontMatter, body: body)
-func Table(body: seq[string]): Block = Block(kind: bkTable, body: body)
-func HTML(body: seq[string]): Block = Block(kind: bkHTML, body: body)
+func Blank*(): Block = Block(kind: bkBlank)
+func Heading*(level: int, heading: string): Block = Block(kind: bkHeading, level: level, heading: heading)
+func Code*(language: string, code: seq[CodeLine]): Block = Block(kind: bkCode, language: if language.strip == "": "gdscript" else: language, code: code)
+func Image*(alt: string, path: string): Block = Block(kind: bkImage, alt: alt, path: path)
+func Shortcode*(name: string, args: seq[string]): Block = Block(kind: bkShortcode, name: name, args: args)
+func ShortcodeFromSeq*(x: seq[string]): Block =
+  if x.len == 0: Shortcode("", @[]) else: Shortcode(x[0], x[1..^1].filter(x => x.len > 0))
+func Paragraph*(body: seq[string]): Block = Block(kind: bkParagraph, body: body)
+func List*(body: seq[string]): Block = Block(kind: bkList, body: body)
+func BlockQuote*(body: seq[string]): Block = Block(kind: bkBlockQuote, body: body)
+func YAMLFrontMatter*(body: seq[string]): Block = Block(kind: bkYAMLFrontMatter, body: body)
+func Table*(body: seq[string]): Block = Block(kind: bkTable, body: body)
+func HTML*(body: seq[string]): Block = Block(kind: bkHTML, body: body)
 
 func render*(b: Block): string =
   case b.kind
     of bkBlank: ""
-    of bkHeading: '#'.repeat(b.level) & " " & b.heading
+    of bkHeading: '#'.repeat(b.level) & SPACE & b.heading
     of bkCode: (@["```" & b.language] & b.code.map(render) & @["```"]).join(NL)
     of bkImage: "![" & b.alt & "](" & b.path & ")"
-    of bkGDQuestShortcode: (@["{%", b.name] & b.args & @["%}"]).join(" ")
+    of bkShortcode: (@["{%", b.name] & b.args & @["%}"]).filter(x => x.len > 0).join(SPACE)
     of bkYAMLFrontMatter: (@["---"] & b.body & @["---"]).join(NL)
     else: b.body.join(NL)
 
@@ -94,18 +92,17 @@ let
 
 
 let
-  nonEmptyline = nonNewLine.atLeast(1).join << eol
-  line = eol | nonEmptyline
-  listLine = (manySpaceOrTab & regex(r"-|([a-z]|[0-9]|#)+\.") & nonEmptyline).join
-  blockQuoteLine = (s(">") & manySpaceOrTab & nonEmptyline).join
+  nonEmptyLine = nonNewLine.atLeast(1).join << eol
+  line = eol | nonEmptyLine
+  listLine = (manySpaceOrTab & regex(r"-|([a-z]|[0-9]|#)+\.") & nonEmptyLine).join
+  blockQuoteLine = (s(">") & manySpaceOrTab & nonEmptyLine).join
   (codeOpenLine, codeCloseLine) = (s("```") >> nonNewLine.many.join << newLine, s("```") << eol)
   yamlOpenClose = (s("---") << eol)
 
 
 let
   blank = eol.result(Blank())
-  heading = ((c('#').atLeast(1) << manySpaceOrTab).join & nonEmptyline).map(x => Heading(x[0].len, x[1]))
-  paragraph = nonEmptyline.atLeast(1).map(Paragraph)
+  heading = ((c('#').atLeast(1) << manySpaceOrTab).join & nonEmptyLine).map(x => Heading(x[0].len, x[1]))
   list = listLine.atLeast(1).map(List)
   blockQuote = blockQuoteLine.atLeast(1).map(BlockQuote)
   image = (
@@ -113,20 +110,23 @@ let
     (s("](") >> ((nonNewLine << !c(')')).many & nonNewLine).join) << c(')') << eol
   ).map(x => Image(x[0], x[1]))
 
-  gdquestShortcodeToken = ((alphanumeric | c("._")).many).join << manySpaceOrTab
-  gdquestShortcode = (
-    manySpaceOrTab >> s("{%") >> manySpaceOrTab >> gdquestShortcodeToken.atLeast(1) << s("%}") << eol
-  ).map(x => GDQuestShortcode(x[0], x[1..^1].filter(x => x.len > 0)))
+  # TODO: preserve codeline shortcode leading whitespace
+  shortcodeToken = (alphanumeric | c("._")).many.join << manySpaceOrTab
+  shortcodeSection* = manySpaceOrTab >> s("{%") >> manySpaceOrTab >> shortcodeToken.atLeast(1) << s("%}") << manySpaceOrTab
+  shortcode = shortcodeSection.map(ShortcodeFromSeq) << eol
+
+  paragraphSection* = (nonNewLine << !s("{%")).many.join
+  paragraph = nonEmptyLine.atLeast(1).map(Paragraph)
 
   lineToCodeLine = proc(x: string): CodeLine =
-    if x.strip.startsWith("{%"): CodeGDQuestShortcode(gdquestShortcode.parse(x).value)
+    if x.strip.startsWith("{%"): CodeShortcode(shortcode.parse(x).value)
     else: CodeRegular(x.strip(false))
   code = (codeOpenLine & (line << !codeCloseLine).many & line << codeCloseLine).map(x => Code(x[0], x[1 .. ^1].map(lineToCodeLine)))
 
   yamlFrontMatter = (yamlOpenClose >> (line << !yamlOpenClose).many & (line << yamlOpenClose)).map(YAMLFrontMatter)
   table = (s("|") & line).join.atLeast(1).map(Table)
   html = (manySpaceOrTab & s("<") & line).join.atLeast(1).map(HTML)
-  parser = (blank | yamlFrontMatter | code | heading | list | blockQuote | html | table | gdquestShortcode | image | paragraph).many
+  parser = (blank | yamlFrontMatter | code | heading | list | blockQuote | html | table | shortcode | image | paragraph).many
 
 
 proc parse*(contents: string): seq[Block] =

commit a2f5f9705ec063ad1af59d9c2040cb2acd7ddf30

fix(formatter): regression from 03992c4f4e skipping paragraphs

src/format.nim
@@ -69,26 +69,12 @@ let
   RegexNumber = re"\d+(D|px)|\d+(x\d+)*"
   RegexHexValue = re"(0x|#)[0-9a-fA-F]+"
   RegexCodeIdentifier = re([PatternFunctionOrConstructorCall, PatternVariablesAndProperties].join("|"))
-  RegexGodotBuiltIns = re(CACHE_GODOT_BUILTIN_CLASSES.join("|"))
+  RegexGodotBuiltIns = re([r"\b(", CACHE_GODOT_BUILTIN_CLASSES.join("|"), r")\b"].join)
   RegexSkip = re"""({%.*?%}|_.+?_|\*\*[^*]+?\*\*|\*[^*]+?\*|`.+?`|".+?"|'.+?'|\!?\[.+?\)|\[.+?\])\s*|\s+|$"""
   RegexStartOfSentence = re"\s*\p{Lu}"
   RegexEndOfSentence = re"[.!?:]\s+"
   RegexFourSpaces = re" {4}"
   RegexCodeCommentSymbol = re"#{3,}|/+"
-
-
-func regexWrap(regexes: seq[Regex], pair: (string, string)): string -> (string, string)
-func regexWrapEach(regexAll, regexOne: Regex; pair: (string, string)): string -> (string, string)
-
-let formatters =
-  { "any": regexWrapEach(RegexKeyboardShortcut, RegexOneKeyboardKey, ("<kbd>", "</kbd>"))
-  , "any": regexWrap(@[RegexFilePath], ("`", "`"))
-  , "any": regexWrap(@[RegexMenuOrPropertyEntry], ("*", "*"))
-  , "any": regexWrap(@[RegexCodeIdentifier, RegexGodotBuiltIns], ("`", "`"))
-  , "any": regexWrap(@[RegexNumber, RegexHexValue], ("`", "`"))
-  , "any": regexWrap(@[RegexOnePascalCaseWordStrict], ("*", "*"))
-  , "skipStartOfSentence": regexWrap(@[RegexCapitalWordSequence, RegexOnePascalCaseWord], ("*", "*"))
-  }
 
 
 type
@@ -127,6 +113,16 @@ func regexWrapEach(regexAll, regexOne: Regex; pair: (string, string)): string ->
       let replaced = replacef(text[0 ..< bound], regexOne, pair[0] & "$1" & pair[1])
       return (replaced, text[bound .. ^1])
     return ("", text)
+
+let formatters =
+  { "any": regexWrapEach(RegexKeyboardShortcut, RegexOneKeyboardKey, ("<kbd>", "</kbd>"))
+  , "any": regexWrap(@[RegexFilePath], ("`", "`"))
+  , "any": regexWrap(@[RegexMenuOrPropertyEntry], ("*", "*"))
+  , "any": regexWrap(@[RegexCodeIdentifier, RegexGodotBuiltIns], ("`", "`"))
+  , "any": regexWrap(@[RegexNumber, RegexHexValue], ("`", "`"))
+  , "any": regexWrap(@[RegexOnePascalCaseWordStrict], ("*", "*"))
+  , "skipStartOfSentence": regexWrap(@[RegexCapitalWordSequence, RegexOnePascalCaseWord], ("*", "*"))
+  }
 
 
 proc formatLine(line: string): string =
@@ -223,6 +219,9 @@ proc formatBlock(mdBlock: Block): string =
 
   of bkList:
     partialResult.add mdBlock.body.formatList
+
+  of bkParagraph:
+    partialResult.add mdBlock.body.map(formatLine)
 
   else:
     partialResult.add(mdBlock.render)