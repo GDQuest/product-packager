b72936478a5036320b5cd794838f435ed86b828f
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2022-03-14 18:17:27 +0200

    fix(formatter): assets path

--- src/md/assets.nim
+++ src/md/assets.nim
@@ -1,6 +1,7 @@
 import std/
   [ algorithm
   , os
+  , strformat
   , strutils
   , sugar
   , tables
@@ -10,18 +11,18 @@ import std/
 const
   XML_EXT = ".xml"
   SVG_EXT = ".svg"
-  DIR_GODOT_ICONS = "../../godot/editor/icons"
-  DIR_GODOT_DOC_CLASSES = "../../godot/doc/classes"
-  DIR_GODOT_MODULES = "../../godot/modules"
+  DIR_GODOT_ICONS = "godot/editor/icons"
+  DIR_GODOT_MODULES = "godot/modules"
+  DIR_GODOT_DOC_CLASSES = "godot/doc/classes"
 
   CACHE_GODOT_BUILTIN_CLASSES* = block:
     var result: seq[string]
 
-    for node in walkDir(DIR_GODOT_DOC_CLASSES, checkDir = true):
+    for node in walkDir(DIR_GODOT_DOC_CLASSES):
       if node.kind == pcFile and node.path.toLower.endsWith(XML_EXT):
         result.add node.path.splitFile.name
 
-    for path in walkDirRec(DIR_GODOT_MODULES, checkDir = true):
+    for path in walkDirRec(DIR_GODOT_MODULES):
       if "doc_classes" in path and path.toLower.endsWith(XML_EXT):
         result.add path.splitFile.name
 
@@ -30,12 +31,12 @@ const
   CACHE_GODOT_ICONS* = block:
     var result: Table[string, string]
 
-    for node in walkDir(DIR_GODOT_ICONS, checkDir = true):
+    for node in walkDir(DIR_GODOT_ICONS):
       if node.kind == pcFile and node.path.toLower.endsWith(SVG_EXT):
-        result[node.path.splitFile.name] = staticRead(node.path)
+        result[node.path.splitFile.name] = staticRead(fmt"../../{node.path}")
 
-    for path in walkDirRec(DIR_GODOT_MODULES, checkDir = true):
+    for path in walkDirRec(DIR_GODOT_MODULES):
       if "icons" in path and path.toLower.endsWith(SVG_EXT):
-        result[path.splitFile.name] = staticRead(path)
+        result[path.splitFile.name] = staticRead(fmt"../../{path}")
 
     result
