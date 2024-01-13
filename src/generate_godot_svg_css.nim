3809e1fabab30916cd041807301de3d99d7b03f3
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-01-13 18:08:01 +0100

    feat: write program to generate css and package svg files for all Godot icons for use on the web

---
+++ src/generate_godot_svg_css.nim
@@ -0,0 +1,40 @@
+## This program bundles the SVG icons and classes from the Godot source code into a single
+## executable file. It then outputs a css file and a folder filled with SVG files to use
+## on websites.
+## 
+## Creates the following files in the current directory:
+## 
+## - godot_icons.css
+## - icons/godot/*.svg
+
+
+import md/assets
+import std/tables
+import std/strformat
+import std/os
+
+when isMainModule:
+    # Generating css
+    # These attributes will make the icons scalable and make them work with a single class name (i-gd-*)
+    var css: string = """[class^='i-gd-'], [class*=' i-gd-']{
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-color: transparent;
+  height: 1em;
+  width: 1em;
+}
+"""
+    for godotClassName in assets.CACHE_GODOT_ICONS.keys():
+        css.add(fmt".i-gd-{godotClassName} {{ background-image: url(/icons/godot/{godotClassName}.svg); }}" & "\n")
+    writeFile("godot_icons.css", css)
+    echo("Wrote godot_icons.css")
+
+    echo(fmt"Writing SVG files to ./icons/godot/...")
+    var svgFolder = "icons/godot"
+    if not os.dirExists(svgFolder):
+        os.createDir(svgFolder)
+    for godotClassName in assets.CACHE_GODOT_ICONS.keys():
+        let svgData = assets.CACHE_GODOT_ICONS[godotClassName]
+        writeFile(fmt"{svgFolder}/{godotClassName}.svg", svgData)
+    echo("Done!")
