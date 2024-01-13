858549a34d9cc3fbc5f26799c2835f8bf0c26073
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-01-13 17:47:13 +0100

    refactor: rename variables

--- src/md/assets.nim
+++ src/md/assets.nim
@@ -9,8 +9,8 @@ import std/
 
 
 const
-  XML_EXT = ".xml"
-  SVG_EXT = ".svg"
+  EXT_XML = ".xml"
+  EXT_SVG = ".svg"
   DIR_GODOT_ICONS = "godot/editor/icons"
   DIR_GODOT_MODULES = "godot/modules"
   DIR_GODOT_DOC_CLASSES = "godot/doc/classes"
@@ -19,11 +19,11 @@ const
     var result: seq[string]
 
     for node in walkDir(DIR_GODOT_DOC_CLASSES):
-      if node.kind == pcFile and node.path.toLower.endsWith(XML_EXT):
+      if node.kind == pcFile and node.path.toLower.endsWith(EXT_XML):
         result.add node.path.splitFile.name
 
     for path in walkDirRec(DIR_GODOT_MODULES):
-      if "doc_classes" in path and path.toLower.endsWith(XML_EXT):
+      if "doc_classes" in path and path.toLower.endsWith(EXT_XML):
         result.add path.splitFile.name
 
     result.sorted((x, y) => cmp(x.len, y.len), Descending)
@@ -32,11 +32,11 @@ const
     var result: Table[string, string]
 
     for node in walkDir(DIR_GODOT_ICONS):
-      if node.kind == pcFile and node.path.toLower.endsWith(SVG_EXT):
+      if node.kind == pcFile and node.path.toLower.endsWith(EXT_SVG):
         result[node.path.splitFile.name] = staticRead(fmt"../{node.path}")
 
     for path in walkDirRec(DIR_GODOT_MODULES):
-      if "icons" in path and path.toLower.endsWith(SVG_EXT):
+      if "icons" in path and path.toLower.endsWith(EXT_SVG):
         result[path.splitFile.name] = staticRead(fmt"../{path}")
 
     result
