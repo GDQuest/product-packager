c6ea475ce121446adb39d3f8e65da520dde8331a
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-08-30 12:10:28 +0200

    fix: fix md/assets module working differently depending on the cwd at compile time

--- mdx-utils/src/md/assets.nim
+++ mdx-utils/src/md/assets.nim
@@ -10,19 +10,23 @@ import std/
 const
   EXT_XML = ".xml"
   EXT_SVG = ".svg"
-  DIR_ROOT = "src/md"
   DIR_GODOT_ICONS = "godot/editor/icons"
   DIR_GODOT_MODULES = "godot/modules"
   DIR_GODOT_DOC_CLASSES = "godot/doc/classes"
-  
+  DIR_THIS = currentSourcePath.parentDir()
+
   CACHE_GODOT_BUILTIN_CLASSES* = block:
     var result: seq[string]
 
-    for node in walkDir(DIR_ROOT / DIR_GODOT_DOC_CLASSES):
+    echo DIR_THIS
+    const DIR_GODOT_CLASSES = DIR_THIS / DIR_GODOT_DOC_CLASSES
+    const DIR_GODOT_MODULES = DIR_THIS / DIR_GODOT_MODULES
+
+    for node in walkDir(DIR_GODOT_CLASSES):
       if node.kind == pcFile and node.path.toLower.endsWith(EXT_XML):
         result.add node.path.splitFile.name
 
-    for path in walkDirRec(DIR_ROOT / DIR_GODOT_MODULES):
+    for path in walkDirRec(DIR_GODOT_MODULES):
       if "doc_classes" in path and path.toLower.endsWith(EXT_XML):
         result.add path.splitFile.name
     result.sorted((x, y) => cmp(x.len, y.len), Descending)
@@ -30,13 +34,13 @@ const
   CACHE_GODOT_ICONS* = block:
     var result: Table[string, string]
 
-    for node in walkDir(DIR_ROOT / DIR_GODOT_ICONS):
+    for node in walkDir(DIR_THIS / DIR_GODOT_ICONS):
       if node.kind == pcFile and node.path.toLower.endsWith(EXT_SVG):
-        result[node.path.splitFile.name] = staticRead(node.path.replace(DIR_ROOT, ""))
+        result[node.path.splitFile.name] = staticRead(node.path.replace(DIR_THIS, ""))
 
-    for path in walkDirRec(DIR_ROOT / DIR_GODOT_MODULES):
+    for path in walkDirRec(DIR_THIS / DIR_GODOT_MODULES):
       if "icons" in path and path.toLower.endsWith(EXT_SVG):
-        result[path.splitFile.name] = staticRead(path.replace(DIR_ROOT, ""))
+        result[path.splitFile.name] = staticRead(path.replace(DIR_THIS, ""))
 
     result
 
