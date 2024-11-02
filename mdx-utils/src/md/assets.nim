1e59ecd4780c449aaea560fb3f67efa5562699a4
Nathan Lovato <nathan@gdquest.com>
2024-11-02 22:43:20 +0100

    add support for groups in godot icons

--- mdx-utils/src/md/assets.nim
+++ mdx-utils/src/md/assets.nim
@@ -1,42 +1,53 @@
-import std/[algorithm, os, strutils, sugar, tables]
+import std/[algorithm, os, strutils, sugar, tables, sets]
 
 const
   EXT_XML = ".xml"
   EXT_SVG = ".svg"
-  DIR_GODOT_ICONS = "godot/editor/icons"
-  DIR_GODOT_MODULES = "godot/modules"
-  DIR_GODOT_DOC_CLASSES = "godot/doc/classes"
   DIR_THIS = currentSourcePath.parentDir()
+  DIR_GODOT_MODULES = DIR_THIS / "godot/modules"
 
-  CACHE_GODOT_BUILTIN_CLASSES* = static:
-    var result: seq[string]
+const CACHE_GODOT_BUILTIN_CLASSES* = static:
+  var result: seq[string]
 
-    echo DIR_THIS
-    const DIR_GODOT_CLASSES = DIR_THIS / DIR_GODOT_DOC_CLASSES
-    const DIR_GODOT_MODULES = DIR_THIS / DIR_GODOT_MODULES
+  echo "Caching Godot builtin classes..."
 
-    for node in walkDir(DIR_GODOT_CLASSES):
-      if node.kind == pcFile and node.path.toLower.endsWith(EXT_XML):
-        result.add node.path.splitFile.name
+  const DIR_GODOT_DOCUMENTATION = DIR_THIS / "godot/doc/classes"
+  for node in walkDir(DIR_GODOT_DOCUMENTATION):
+    if node.kind == pcFile and node.path.toLower.endsWith(EXT_XML):
+      result.add node.path.splitFile.name
 
-    for path in walkDirRec(DIR_GODOT_MODULES):
-      if "doc_classes" in path and path.toLower.endsWith(EXT_XML):
-        result.add path.splitFile.name
-    result.sorted((x, y) => cmp(x.len, y.len), Descending)
+  for path in walkDirRec(DIR_GODOT_MODULES):
+    if "doc_classes" in path and path.toLower.endsWith(EXT_XML):
+      result.add path.splitFile.name
+  result.sorted((x, y) => cmp(x.len, y.len), Descending)
 
-  CACHE_GODOT_ICONS* = static:
-    var result: Table[string, string]
+const CACHE_GODOT_ICONS* = block:
+  var result: Table[string, string]
 
-    for node in walkDir(DIR_THIS / DIR_GODOT_ICONS):
-      if node.kind == pcFile and node.path.toLower.endsWith(EXT_SVG):
-        result[node.path.splitFile.name] = staticRead(node.path.replace(DIR_THIS, ""))
+  echo "Caching Godot icons..."
 
-    for path in walkDirRec(DIR_THIS / DIR_GODOT_MODULES):
-      if "icons" in path and path.toLower.endsWith(EXT_SVG):
-        result[path.splitFile.name] = staticRead(path.replace(DIR_THIS, ""))
+  const DIR_GODOT_ICONS = DIR_THIS / "godot/editor/icons"
+  for node in walkDir(DIR_GODOT_ICONS):
+    if node.kind == pcFile and node.path.toLower.endsWith(EXT_SVG):
+      result[node.path.splitFile.name] = staticRead(node.path.replace(DIR_THIS, ""))
 
-    result
+  for path in walkDirRec(DIR_GODOT_MODULES):
+    if "icons" in path and path.toLower.endsWith(EXT_SVG):
+      result[path.splitFile.name] = staticRead(path.replace(DIR_THIS, ""))
 
-  CACHE_BLACKLIST* = ["2D", "3D", "Godot", "GDScript", "GDQuest", "UI"].sorted(
-    (x, y) => cmp(x.len, y.len), Descending
-  )
+  result
+
+const CACHE_EDITOR_ICONS* = block:
+  var icons_seq = newSeq[string]()
+  echo "Caching editor icons..."
+
+  for icon in CACHE_GODOT_ICONS.keys:
+    if icon notin CACHE_GODOT_BUILTIN_CLASSES:
+      icons_seq.add(icon)
+
+  icons_seq.toHashSet()
+
+echo "Done caching."
+const CACHE_BLACKLIST* = ["2D", "3D", "Godot", "GDScript", "GDQuest", "UI"].sorted(
+  (x, y) => cmp(x.len, y.len), Descending
+)
