d9e777b7291af0e3233cefd2900078ca675ef27a
Nathan Lovato <nathan@gdquest.com>
2024-11-19 23:04:50 +0100

    c

--- mdx-utils/src/md/preprocessor_rewrite.nim
+++ mdx-utils/src/md/preprocessor_rewrite.nim
@@ -1,5 +1,6 @@
-import std/[nre, tables]
+import std/[nre, strformat, strutils, tables, options, os, terminal]
 import assets
+import utils
 import ../types
 import ../image_size
 
@@ -17,24 +18,99 @@ type
 ## all files to group them at the end of the program's execution.
 var preprocessorErrorMessages*: seq[string] = @[]
 
-const
-  REGEX_ANCHOR_LINE = re"(?m)(?s)\h*(#|\/\/)\h*(ANCHOR|END).*?(\v|$)"
+let
+  regexAnchorLine = re"(?m)(?s)\h*(#|\/\/)\h*(ANCHOR|END).*?(\v|$)"
+
+proc preprocessVideoFile(match: RegexMatch, context: HandlerContext): string =
+  ## Replaces the relative input video path with an absolute path in the website's public directory.
+  let
+    captures = match.captures.toTable()
+    src = captures["src"]
+    before = captures.getOrDefault("before", " ").strip()
+    after = captures.getOrDefault("after", " ").strip()
+    outputPath = context.outputDirPath / src
+  result = "<VideoFile " & before & " src=\"" & outputPath & "\"" & " " & after & "/>"
+
+proc replaceGodotIcon(match: RegexMatch, context: HandlerContext): string =
+  ## Replaces a Godot class name in inline code formatting with an icon component followed by the class name.
+  ## TODO: replace with new icon component format.
+  let className = match.captures.toTable()["class"].strip(chars = {'`'})
+  if className in CACHE_GODOT_ICONS:
+    result = "<IconGodot name=\"" & className & "\"/> " & match.match
+  else:
+    echo(fmt"Couldn't find icon for `{className}`. Skipping...")
+    result = match.match
+
+proc preprocessIncludeComponent(match: RegexMatch, context: HandlerContext): string =
+  ## Replaces the Include shortcode with the contents of the section of a file or full file it points to.
+  let args = match.captures.toTable()
+  let includeFileName = cache.findCodeFile(args["file"])
+
+  try:
+    result = readFile(includeFileName)
+    if "anchor" in args:
+      let
+        anchor = args["anchor"]
+        regexAnchor = re(
+          fmt(
+            r"(?s)\h*(?:#|\/\/)\h*ANCHOR:\h*\b{anchor}\b\h*\v(?P<contents>.*?)\s*(?:#|\/\/)\h*END:\h*\b{anchor}\b"
+          )
+        )
+
+      let anchorMatch = result.find(regexAnchor)
+      if anchorMatch.isSome():
+        let
+          anchorCaptures = anchorMatch.get.captures.toTable()
+          output = anchorCaptures["contents"]
+          lines = output.splitLines()
+        var prefixedLines: seq[string] = @[]
+
+        # If we're in a code block, preserve indentation
+        let prefix = args.getOrDefault("prefix", "")
+        for line in lines:
+          prefixedLines.add(prefix & line)
+        result = prefixedLines.join("\n")
+      else:
+        let errorMessage =
+          fmt"Can't find matching contents for anchor {anchor} in file {includeFileName}."
+        stderr.styledWriteLine(fgRed, errorMessage)
+        preprocessorErrorMessages.add(errorMessage)
+        return match.match
+
+    # Clean up anchor markers and extra newlines
+    result = result.replace(regexAnchorLine, "").strip(chars = {'\n'})
+
+  except IOError:
+    let errorMessage =
+      fmt"Failed to read include file: {includeFileName}"
+    stderr.styledWriteLine(fgRed, errorMessage)
+    preprocessorErrorMessages.add(errorMessage)
+    return match.match
+
+proc preprocessMarkdownImage(match: RegexMatch, context: HandlerContext): string =
+  ## Replaces the relative input image path with an absolute path in the website's public directory.
+  ## Also gives the image a class depending on its aspect ratio.
+  let
+    captures = match.captures.toTable()
+    alt = captures["alt"].replace("\"", "'")
+    relpath = captures["path"]
+    outputPath = context.outputDirPath / relpath
+    dimensions = getImageDimensions(context.inputDirPath / relpath)
+  let
+    ratio = dimensions.width / dimensions.height
+    className =
+      if ratio < 1.0: "portrait-image"
+      elif ratio == 1.0: "square-image"
+      else: "landscape-image"
+  result = fmt"""<PublicImage src="{outputPath}" alt="{alt}" className="{className}" width="{dimensions.width}" height="{dimensions.height}"/>"""
 
 const
   PATTERNS = {
     '`': @[
       PatternHandler(
         # TODO: add all node classes from assets module
-        pattern: re"(`(?P<class>Node|Control|Sprite2D)`)"),
-        handler: proc(match: RegexMatch, context: HandlerContext): string =
-          ## Replaces a Godot class name in inline code formatting with an icon component followed by the class name.
-          ## TODO: replace with new icon component format.
-          let className = match.captures.toTable()["class"].strip(chars = {'`'})
-          if className in CACHE_GODOT_ICONS:
-            result = "<IconGodot name=\"" & className & "\"/> " & match.match
-          else:
-            info(fmt"Couldn't find icon for `{className}`. Skipping...")
-            result = match.match
+        pattern: ["(`(?P<class>", CACHE_GODOT_BUILTIN_CLASSES.join("|"), ")`)"].join.re(),
+        handler: replaceGodotIcon,
       )
     ],
     '<': @[
@@ -43,87 +119,19 @@ const
         pattern: re(
           r"""(?P<prefix>.*?)?< *Include\s+file=["'](?P<file>.+?\.[a-zA-Z0-9]+)["']\s*(?:anchor=["'](?P<anchor>\w+)["'])?\s*/>"""
         ),
-        handler: proc handleIncludeShortcode(match: RegexMatch, context: HandlerContext): string =
-          ## Replaces the Include shortcode with the contents of the section of a file or full file it points to.
-          let args = match.captures.toTable()
-          let includeFileName = cache.findCodeFile(args["file"])
-
-          try:
-            result = readFile(includeFileName)
-
-            if "anchor" in args:
-              let
-                anchor = args["anchor"]
-                regexAnchor = re(
-                  fmt(
-                    r"(?s)\h*(?:#|\/\/)\h*ANCHOR:\h*\b{anchor}\b\h*\v(?P<contents>.*?)\s*(?:#|\/\/)\h*END:\h*\b{anchor}\b"
-                  )
-                )
-
-              let anchorMatch = result.find(regexAnchor)
-              if anchorMatch.isSome():
-                let
-                  anchorCaptures = anchorMatch.get.captures.toTable()
-                  output = anchorCaptures["contents"]
-                  lines = output.splitLines()
-                var prefixedLines: seq[string] = @[]
-
-                # If we're in a code block, preserve indentation
-                let prefix = if "prefix" in args: args["prefix"] else: ""
-                for line in lines:
-                  prefixedLines.add(prefix & line)
-                result = prefixedLines.join("\n")
-              else:
-                let errorMessage =
-                  fmt"Can't find matching contents for anchor {anchor} in file {includeFileName}."
-                stderr.styledWriteLine(fgRed, errorMessage)
-                preprocessorErrorMessages.add(errorMessage)
-                return match.match
-
-            # Clean up anchor markers and extra newlines
-            result = result.replace(REGEX_ANCHOR_LINE, "").strip(chars = {'\n'})
-
-          except IOError:
-            let errorMessage =
-              fmt"Failed to read include file: {includeFileName}"
-            stderr.styledWriteLine(fgRed, errorMessage)
-            preprocessorErrorMessages.add(errorMessage)
-            return match.match
+        handler: preprocessIncludeComponent,
       ),
       PatternHandler(
         # Video file component
-        pattern: re"<VideoFile\s*(?P<before>.*?)?src=[\"'](?P<src>[^\"']+)[\"'](?P<after>.*?)?/>",
-        handler: proc(match: RegexMatch, context: HandlerContext): string =
-          ## Replaces the relative input video path with an absolute path in the website's public directory.
-          let
-            captures = match.captures.toTable()
-            src = captures["src"]
-            before = captures.getOrDefault("before", " ").strip()
-            after = captures.getOrDefault("after", " ").strip()
-            outputPath = context.outputDirPath / src
-          result = "<VideoFile " & before & " src=\"" & outputPath & "\"" & " " & after & "/>"
+        pattern: re"""<VideoFile\s*(?P<before>.*?)?src=["'](?P<src>[^\"']+)["'](?P<after>.*?)?/>""",
+        handler: preprocessVideoFile,
       )
     ],
     '!': @[
       PatternHandler(
         # Markdown images
         pattern: re"!\[(?P<alt>.*?)\]\((?P<path>.+?)\)",
-        handler: proc handleMarkdownImage(match: RegexMatch, context: HandlerContext): string =
-          ## Replaces the relative input image path with an absolute path in the website's public directory.
-          ## Also gives the image a class depending on its aspect ratio.
-          let
-            captures = match.captures.toTable()
-            alt = captures["alt"].replace("\"", "'")
-            relpath = captures["path"]
-            outputPath = context.outputDirPath / relpath
-            dimensions = image_size.getImageDimensions(context.inputDirPath / relpath)
-          let
-            ratio = dimensions.width / dimensions.height
-            className =
-              if ratio < 1.0: "portrait-image"
-              elif ratio == 1.0: "square-image"
-              else: "landscape-image"
-          result = fmt"""<PublicImage src="{outputPath}" alt="{alt}" className="{className}" width="{dimensions.width}" height="{dimensions.height}"/>"""
+        handler: preprocessMarkdownImage,
       )
     ]
   }.toTable()
