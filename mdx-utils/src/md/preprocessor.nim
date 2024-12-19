be2d7bc46ebbec26c206f7e814991abd7d4114a1
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-12-19 23:25:02 +0100

    fix: return an empty string when including a nonexistent anchor
    fix #67

--- mdx-utils/src/md/preprocessor.nim
+++ mdx-utils/src/md/preprocessor.nim
@@ -14,6 +14,7 @@ import assets
 import utils
 import ../settings
 import ../image_size
+import ../gdscript/parser_gdscript
 when compileOption("profiler"):
   import std/nimprof
 
@@ -187,10 +188,10 @@ proc preprocessIncludeComponent(match: RegexMatch, context: HandlerContext): str
               result.replace(searchAndReplace.source, searchAndReplace.replacement)
       else:
         let errorMessage =
-          fmt"Can't find matching contents for anchor {anchor} in file {includeFileName}."
+          fmt"Can't find matching contents for anchor {anchor} in file {includeFileName}. Nothing will be included."
         stderr.styledWriteLine(fgRed, errorMessage)
         preprocessorErrorMessages.add(errorMessage)
-        return match.match
+        return ""
 
     # Clean up anchor markers and extra newlines
     result = result.replace(regexAnchorLine, "").strip(chars = {'\n'})
